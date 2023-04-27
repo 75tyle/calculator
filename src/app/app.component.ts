2ad38cc014de405bf7b070801d91614cb9bbc207
sanghpal <kamblesanghpal2003@gmail.com>
2023-04-27 22:13:39 +0530

    wesdfhjk

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  number:any
+  submit(){
+    let splitted = this.number.split()
+   console.log(this.number)
+   console.log(splitted)
+   const usingSplit = this.number.split('');
+const usingSpread = [...this.number];
+const usingArrayFrom = Array.from(this.number);
+const usingObjectAssign = Object.assign([], this.number);
+console.log(usingObjectAssign)
+  }
+  ngOnInit(): void {
+    console.log(this.number)
+
+  }
   title = 'calculator';
+ 
 }
